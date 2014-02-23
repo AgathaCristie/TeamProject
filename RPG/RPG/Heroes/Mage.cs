23ae3bfb88e246246556ffa0a3e14cb4a0864e23
martinapetrova <martina.petrova.779@gmail.com>
2014-02-23 20:45:15 +0200

    Add class Mage
    Add class mage and new sprites

--- RPG/RPG/Heroes/Mage.cs
+++ RPG/RPG/Heroes/Mage.cs
@@ -1,31 +1,30 @@
 ﻿namespace RPG.Heroes
 {
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Content;
+    using Microsoft.Xna.Framework.Graphics;
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
-    using Microsoft.Xna.Framework;
-    using Microsoft.Xna.Framework.Content;
-    using Microsoft.Xna.Framework.Graphics;
-    using Microsoft.Xna.Framework.Input;
 
-    public class Hero : Player, IHeroes
+    public class Mage : Player, IHeroes
     {
         //Fields
         private int windowHeight;
         private int windowWidth;
 
         //Constructor
-        public Hero(ContentManager manager, int width, int height)
+        public Mage(ContentManager manager, int width, int height)
         {
             windowWidth = width;
             windowHeight = height;
 
             //Loads The images from the files when the hero is going right
-            this.Images[0] = manager.Load<Texture2D>("sprites\\HeroMoves\\HeroMove1");
-            this.Images[1] = manager.Load<Texture2D>("sprites\\HeroMoves\\HeroMove2");
-            this.Images[2] = manager.Load<Texture2D>("sprites\\HeroMoves\\HeroMove3");
-            this.Images[3] = manager.Load<Texture2D>("sprites\\HeroMoves\\HeroMove4");
+            this.Images[0] = manager.Load<Texture2D>("sprites\\MageMove\\MageMove1");
+            this.Images[1] = manager.Load<Texture2D>("sprites\\MageMove\\MageMove2");
+            this.Images[2] = manager.Load<Texture2D>("sprites\\MageMove\\MageMove3");
+            this.Images[3] = manager.Load<Texture2D>("sprites\\MageMove\\MageMove4");
 
             //Loads The images from the files when the hero is going left
             this.ImagesLeft[0] = manager.Load<Texture2D>("sprites\\HeroMoves\\HeroMoveLeft1");

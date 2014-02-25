25412487dc286643441df411aef662099f4d19de
ksevery <konstantinsevery@gmail.com>
2014-02-25 22:05:27 +0200

    Basis for inventory menu

---
+++ RPG/RPG/InventoryMenu.cs
@@ -0,0 +1,42 @@
+﻿namespace RPG
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Content;
+    using Microsoft.Xna.Framework.Graphics;
+
+    public class InventoryMenu : BaseGameScreen
+    {
+
+
+        public InventoryMenu(Game1 game, SpriteBatch spriteBatch) :
+            base(game, spriteBatch)
+        {
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+
+    }
+}
